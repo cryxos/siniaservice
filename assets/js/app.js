2086516466d5aca0433b02cc6e10c4fb5dd9de11
crixus <cristhian13sys@gmail.com>
2020-07-07 12:17:25 -0500

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,55 @@
+ 
 $(document).ready(function(){
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav();   
+    $('#publicaciones').toggleClass("mostrar");
+    $('#normas').toggleClass("mostrar");
+    $('#indicadores').toggleClass("mostrar");
+    $('#series').toggleClass("mostrar");
+    $('#datos').toggleClass("mostrar");
 });
-       
+ 
+
+function publicaciones(){ 
+    $('#publicaciones').toggleClass("mostrar");
+
+    $('#normas').addClass("mostrar");
+    $('#indicadores').addClass("mostrar");
+    $('#series').addClass("mostrar");
+    $('#datos').addClass("mostrar");
+} 
+ 
+function normas(){
+    $('#normas').toggleClass("mostrar");
+
+    $('#publicaciones').addClass("mostrar");
+    $('#indicadores').addClass("mostrar");
+    $('#series').addClass("mostrar");
+    $('#datos').addClass("mostrar");
+}
+
+function indicadores(){
+    $('#indicadores').toggleClass("mostrar");
+
+    $('#publicaciones').addClass("mostrar");
+    $('#normas').addClass("mostrar"); 
+    $('#series').addClass("mostrar");
+    $('#datos').addClass("mostrar");
+}   
+
+function series(){
+    $('#series').toggleClass("mostrar");
+
+    $('#publicaciones').addClass("mostrar");
+    $('#normas').addClass("mostrar");  
+    $('#indicadores').addClass("mostrar");
+    $('#datos').addClass("mostrar");
+}
+
+function datos(){
+    $('#datos').toggleClass("mostrar");
+
+    $('#publicaciones').addClass("mostrar");
+    $('#normas').addClass("mostrar");  
+    $('#indicadores').addClass("mostrar");
+    $('#series').addClass("mostrar");    
+}
